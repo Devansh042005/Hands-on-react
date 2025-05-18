8269b74459fc42c8823ec3efd3161c0ed63939c4
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-05-18 19:52:40 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Accordian from './components/Accordian'
+import RandomColor from './components/random-color'
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <Accordian/>
+      <RandomColor/>
+    </div>
+  )
+}
+
+export default App
