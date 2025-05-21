c6ac6cfcf38bdb1ae2823b1a700e0e40af79be5c
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-05-21 23:08:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Accordian from './components/Accordian'
-import RandomColor from './components/random-color'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Accordian from "./components/Accordian";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 
 function App() {
-  
-
   return (
-    <div className='App'>
-      <Accordian/>
-      <RandomColor/>
+    <div className="App">
+      {/* <Accordian/>
+      <RandomColor/> */}
+      <StarRating noOfStars={10} />
+      <ImageSlider url={"https://picsum.photos/v2/list"} page = {'1'} limit={"10"} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
